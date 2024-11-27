e0ba565d5222bd267355be0536bc3548e0baca56
김민재 <redvelvet0625@gmail.com>
2024-11-27 12:47:19 +0900

    create Price Entity

--- src/main/java/com/springboot/task04/entity/Price.java
+++ src/main/java/com/springboot/task04/entity/Price.java
@@ -1,4 +1,36 @@
 package com.springboot.task04.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
+
